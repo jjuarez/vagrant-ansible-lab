b50dc42a7815d932e43cc454f8f5af8423d03420
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2021-06-26 13:23:24 +0200

    Old ansible installation script

--- provision/scripts/ansible.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu -o pipefail
-
-apt-get update -qq &&
-apt-get install -y -qq software-properties-common &&
-add-apt-repository -y --update ppa:ansible/ansible
-apt-get install -y -qq ansible
